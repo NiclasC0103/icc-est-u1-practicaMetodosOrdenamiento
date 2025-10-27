c61734e615fcce323cee4fd0a9181a4170dea199
Nicolas Cornejo <Nicolas>
2025-10-27 12:50:48 -0500

    Practica Laboratorio en Clases

--- src/SortSeleccion.java
+++ src/SortSeleccion.java
@@ -1,3 +1,52 @@
 public class SortSeleccion {
-    
+    public SortSeleccion(){
+        System.out.println("Se creó mi clase SortSeleccion\n");
+    }
+    public void sortAscendente(int[] numeros){
+        int len = numeros.length;
+        int aux;
+        for (int i = 0; i < len; i++) {
+            int ind=i;
+            for (int j = i+1; j < len; j++) {
+                if (numeros[j]<numeros[ind]) {
+                    ind=j;
+                }
+            }
+            if (i != ind) {
+                aux= numeros[ind];
+                numeros[ind]=numeros[i];
+                numeros[i]=aux;
+            }
+        }
+    }
+    public void sortDescendente(int[] numeros){
+        int len = numeros.length;
+        int aux;
+        for (int i = 0; i < len; i++) {
+            int ind=i;
+            for (int j = i+1; j < len; j++) {
+                if (numeros[j]>numeros[ind]) {
+                    ind=j;
+                }
+            }
+            if (i != ind) {
+                aux= numeros[ind];
+                numeros[ind]=numeros[i];
+                numeros[i]=aux;
+            }
+        }
+    }
+    public void printArray(int[] numeros){
+        int len = numeros.length;
+        for(int i=0; i<len; i++)
+        {
+            if (i==0) {
+                System.out.print("["+numeros[i]+",");
+            } else if(i==len-1) {
+                System.out.println(numeros[i]+"]");
+            } else {
+                System.out.print(numeros[i]+",");
+            }
+        }
+    }
 }
